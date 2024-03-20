abe1e4712bf2822038bb55258970987563a9f0f5
RickyT <tortoledoricardo@gmail.com>
2024-03-20 03:49:51 +0100

    proyecto

--- app/src/main/java/com/example/proyecto_a/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/proyecto_a/fragments/HomeFragment.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.example.proyecto_a.activities.CategoryMealsActivity
 import com.example.proyecto_a.activities.MealActivity
+import com.example.proyecto_a.adapters.CategoriesAdapter
 import com.example.proyecto_a.adapters.MostPopularAdapter
 import com.example.proyecto_a.databinding.FragmentHomeBinding
 import com.example.proyecto_a.pojo.MealsByCategory
@@ -20,11 +24,12 @@ class HomeFragment : Fragment() {
     private lateinit var homeMvvm: HomeViewModel
     private lateinit var randomMeal:Meal
     private lateinit var popularItemsAdapter:MostPopularAdapter
-
+    private lateinit var categoriesAdapter: CategoriesAdapter
     companion object{
         const val MEAL_ID = "com.example.proyecto_a.fragments.idMeal"
         const val MEAL_NAME = "com.example.proyecto_a.fragments.nameMeal"
         const val MEAL_THUMB = "com.example.proyecto_a.fragments.thumbMeal"
+        const val CATEGORY_NAME = "com.example.proyecto_a.fragments.categoryNamew"
 
 
     }
@@ -58,6 +63,38 @@ class HomeFragment : Fragment() {
         observePopularItemsLiveData()
         onPopularItemClick()
 
+        homeMvvm.getCategories()
+        observeCategoriesLiveData()
+
+        prepareCategoriesRecyclerView()
+        homeMvvm.getCategories()
+        observeCategoriesLiveData()
+        onCategoryClick()
+
+    }
+
+    private fun onCategoryClick() {
+        categoriesAdapter.onItemClick = { category ->
+            val intent =Intent(activity,CategoryMealsActivity::class.java)
+            intent.putExtra(CATEGORY_NAME,category.strCategory)
+            startActivity(intent)
+        }
+    }
+
+    private fun prepareCategoriesRecyclerView() {
+        categoriesAdapter = CategoriesAdapter()
+        binding.recViewCategories.apply {
+            layoutManager = GridLayoutManager(context,3,GridLayoutManager.VERTICAL,false)
+            adapter = categoriesAdapter
+        }
+    }
+
+    private fun observeCategoriesLiveData() {
+        homeMvvm.observeCategoriesLiveData().observe(viewLifecycleOwner, Observer { categories->
+            categories.forEach{category ->
+                categoriesAdapter.setCategoryList(categories)
+            }
+        })
     }
 
     private fun onPopularItemClick() {
